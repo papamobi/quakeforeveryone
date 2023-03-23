1edc07747ec22d7323d86ef121f5fdfff4e83330
papamobi <78706032+papamobi@users.noreply.github.com>
2023-03-23 02:19:20 +0000

    Add files via upload

---
+++ Quake3Plus/nlq3start.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# quick starting a quake 3 dedicated server
+/usr/lib/ioquake3/ioq3ded +com_hunkmegs 96 +set net_port 27960 +set fs_game q3plus +exec server.cfg +set sv_hostname "^1QFE ^4FreezeTag ^7NL"
